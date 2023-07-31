cd4d6d56a0bcdfc841978d15f688ee43df1d69d6
Mariano <devsiderio@tutanota.com>
2023-07-31 11:28:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  const nombre1 = "My";
+  const nombre2 = "Tinerary";
+
+  return (
+    <div className="w-full min-h-screen bg-red-500">
+      <header className="h-[87px] bg-red-200 flex justify-center items-center">
+        esto va a ser la barra de navegación
+      </header>
+      {/* {console.log(nombre2)} */}
+      {/* esto es un comentario */}
+      <main className="grow">{`${nombre1} ${nombre2}`}</main>
+      <footer>esto va a ser el footer</footer>
+    </div>
+  );
+}
+
+export default App;
